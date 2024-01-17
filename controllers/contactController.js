4e7762f61d0e08761b70672389bf2ad15a438d0d
Vee-keme <veliaprecious@gmail.com>
2024-01-17 05:46:29 +0100

    add express-async-handler for async operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,17 @@
+//express async handler is a replacement for try-catch blocks to check for errors in async operations
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all Contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get all contacts" });
-};
+});
 
 //@desc Create New Contacts
 //@route POST /api/contacts/:id
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log("the request body is:", req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
@@ -16,28 +19,28 @@ const createContact = (req, res) => {
     throw new Error("All fields are mandatory");
   }
   res.status(201).json({ message: "create contact" });
-};
+});
 
 //@desc  Get a single Contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `get contact for ${req.params.id}` });
-};
+});
 
 //@desc  Update a single Contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update contact for ${req.params.id}` });
-};
+});
 
 //@desc  Delete a single Contact
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
